89fbea99c5db4eda7301f7cb29ff35b08807a00a
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-12-05 13:16:58 +0530

    Added demo for Context with cancel function

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"strconv"
 )
 
 func main() {
@@ -26,4 +27,13 @@ func main() {
 	log.Print(ctx3)
 	log.Print("Value for the key \"mykey\":")
 	log.Print(ctx3.Value("mykey"))
+
+	log.Print("")
+
+	log.Print("Example #4: Simple Background context with Cancel Function")
+	ctx4, cancelFunc := context.WithCancel(ctx1)
+	log.Print(ctx4)
+	cancelFunc() // Canceling the context by calling cancel function
+	log.Print(ctx4.Err())
+	log.Print("Is canceled : " + strconv.FormatBool(ctx4.Err() == context.Canceled))
 }
